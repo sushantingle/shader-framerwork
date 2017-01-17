24b0328893738d35f08536d3a17b07678def2235
Sushant Ingle <sushant.ingle@ubisoft.com>
2017-01-17 22:26:44 +0530

    Added diffuse shader

---
+++ ShaderFramework/source/app/shader/ShaderDiffuse.cpp
@@ -0,0 +1,36 @@
+#include "ShaderDiffuse.hpp"
+
+void ShaderDiffuse::init() {
+	glEnable(GL_DEPTH_TEST);
+
+	// Set shader parameters
+	setShader("diffuse/diffuse.vert", "diffuse/diffuse.frag");
+}
+
+void ShaderDiffuse::uninit() {
+	glDisable(GL_DEPTH_TEST);
+
+	// remove current active shader
+	removeShader();
+}
+
+void ShaderDiffuse::update() {
+	m_rotationAngle += 0.1f;
+}
+
+void ShaderDiffuse::render() {
+
+	glLoadIdentity();
+
+	gluLookAt(m_cameraPosition.x, 1.0f, m_cameraPosition.z,
+		m_cameraPosition.x + m_cameraRotation.x, 1.0f, m_cameraPosition.z + m_cameraRotation.z,
+		0.0f, 1.0f, 0.0f);
+
+	glRotatef(m_cameraAngle, 0.0f, 1.0f, 0.0f);
+
+	glPushMatrix();
+		glTranslated(0.0f, 0.0f, -50.0f);
+		glutSolidSphere(10.0f, 100, 100);
+	glPopMatrix();
+
+}
---
+++ ShaderFramework/source/app/shader/ShaderDiffuse.hpp
@@ -0,0 +1,22 @@
+#ifndef __SHADERDIFFUSE_HPP__
+#define __SHADERDIFFUSE_HPP__
+
+#include "../framework/shader/ShaderInterface.hpp"
+
+class ShaderDiffuse : public sf::ShaderInterface {
+
+private:
+	void init() override;
+	void uninit() override;
+
+	void update() override;
+	void render() override;
+
+	// Member Variables
+	float m_rotationAngle;
+
+public:
+	ShaderDiffuse():m_rotationAngle(0) {}
+	~ShaderDiffuse() {}
+};
+#endif
